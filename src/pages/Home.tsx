c005c38427390b73a263d0733abf10ff3b7cc528
duongdk099 <52460163+duongdk099@users.noreply.github.com>
2023-07-05 17:28:18 +0200

    filter for home page: done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,50 @@
-import RecipeList from '@/components/recipe/RecipeList'
-import './base-page.scss'
-import { useState } from 'react'
+import RecipeList from "@/components/recipe/RecipeList";
+import "./base-page.scss";
+import { useState } from "react";
+import Filter from "@/components/Filter/Filter";
 
 const Home = () => {
-  const [searchRecipe, setSearchRecipe] = useState('')
+  const [searchRecipe, setSearchRecipe] = useState("");
+  const [filter, setFilter] = useState("");
+  const [selectedOption, setSelectedOption] = useState("option1");
 
   const handleClick = () => {
     //
-  }
+  };
+
+  const handleFilterClick = () => {
+    // Filter logic based on selectedOption and filter value
+    console.log('Selected option:', selectedOption);
+    console.log('Filter value:', filter);
+  };
+
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value);
+  };
 
   return (
     <div className="page">
       <div className="search">
-        <input value={searchRecipe} onChange={(e) => setSearchRecipe(e.target.value)} placeholder='couscous' />
-        <button className='' onClick={handleClick}>rechercher</button>
+        <input
+          value={searchRecipe}
+          onChange={(e) => setSearchRecipe(e.target.value)}
+          placeholder="couscous"
+        />
+        <button className="" onClick={handleClick}>
+          rechercher
+        </button>
       </div>
+
+      <Filter
+        selectedOption={selectedOption}
+        filter={filter}
+        onOptionChange={handleOptionChange}
+        onFilterChange={(e) => setFilter(e.target.value)}
+        onFilterClick={handleFilterClick}
+      />
       <RecipeList />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
