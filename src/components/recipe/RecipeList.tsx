8e7aafd2f2ad94c45a9699993da11f54351c42f3
duongdk099 <52460163+duongdk099@users.noreply.github.com>
2023-07-06 11:12:08 +0200

    Committing local changes to RecipeList.tsx

--- src/components/recipe/RecipeList.tsx
+++ src/components/recipe/RecipeList.tsx
@@ -1,18 +1,55 @@
-import { useRecipes } from '@/contexts/Recipe'
-import './RecipeList.scss'
-import RecipeDetail from './RecipeDetail'
+import React, { useState } from 'react';
+import { useRecipes } from '@/contexts/Recipe';
+import RecipeDetail from './RecipeDetail';
 
-const RecipeList = () => {
-  const { recipes } = useRecipes()
+interface RecipeListProps {
+  filterOption: string;
+  filterValue: string;
+}
+
+const RecipeList: React.FC<RecipeListProps> = ({ filterOption, filterValue }) => {
+  const { recipes } = useRecipes();
+  const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+
+  React.useEffect(() => {
+    // Filter recipes based on the selected option and filter value
+    const filtered = recipes.filter((recipe) => {
+      const { title, description, difficulty, tag, preparationTime } = recipe;
+  
+      if (!filterValue) {
+        // If filterValue is null or empty, don't apply any filtering
+        return true;
+      }
+  
+      switch (filterOption) {
+        case 'Title':
+          return title.toLowerCase().includes(filterValue.toLowerCase());
+        case 'Description':
+          return description?.toLowerCase().includes(filterValue.toLowerCase());
+        case 'Difficulty':
+          return difficulty == Number(filterValue);
+        case 'Tag':
+          return tag.toLowerCase().includes(filterValue.toLowerCase());
+        case 'Preparation Time':
+          return preparationTime == Number(filterValue);
+        default:
+          return true;
+      }
+    });
+  
+    setFilteredRecipes(filtered);
+  }, [filterOption, filterValue, recipes]);
+  
+
   return (
     <div>
       <div className="list">
-        {recipes.map((recipe) => (
+        {filteredRecipes.map((recipe) => (
           <RecipeDetail recipe={recipe} key={recipe.id} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecipeList
+export default RecipeList;
